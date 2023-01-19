337567d2cefa89d777538741e1bd67cd89912f61
sunil <msunilhyd@gmail.com>
2023-01-18 16:14:44 -0800

    Generic Commit

--- leetcode/src/main/java/com/sunil/leetcode/TwoSum.java
+++ leetcode/src/main/java/com/sunil/leetcode/TwoSum.java
@@ -1,18 +1,32 @@
 package com.sunil.leetcode;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class TwoSum {
-
+    public static void main(String[] args) {
+        Solution soln = new Solution();
+        Integer[] inputArr = { 1, 2, 5, 7, 8, 4};
+        List<Integer> inputList = Arrays.asList(inputArr);
+        System.out.println(soln.twoSum(inputList, 9));
+    }
 }
 
 class Solution {
 
-    public void twoSum(List<Integer> inputList) {
+    public List<Integer> twoSum(List<Integer> inputList, int target) {
 
         Map<Integer, Integer> prevMap = new HashMap<Integer, Integer>();
 
+        for (int i=0; i<inputList.size(); i++) {
+           int curVal = inputList.get(i);
+           int diff = target - curVal;
+
+           if (prevMap.containsKey(curVal)) {
+               System.out.println(curVal +", "+ diff);
+           }
+           prevMap.put(diff, curVal);
+        }
+
+        return Arrays.asList(0);
     }
 }

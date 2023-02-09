011e73e26735d198223af3ab65765f7a65445de9
sunil <msunilhyd@gmail.com>
2023-02-09 11:14:50 -0800

    Generic Commit

--- yt-clone/src/components/videoMetaData/VideoMetaData.js
+++ yt-clone/src/components/videoMetaData/VideoMetaData.js
@@ -28,6 +28,64 @@ const videoMetaData = ({ video: { snippet, statistics }, videoId }) => {
   )
 
   useEffect(() => {
-    dispatch(getC)
-  })
+    dispatch(getChannelDetails(channelId))
+    dispatch(checkSubscriptionStatus(channelId))
+  }, [dispatch, channelId])
+
+  return (
+    <div className='py-2 videoMetaData'>
+      <div className='videoMetaData__top'>
+        <h5>{title}</h5>
+        <div className='py-1 d-flex justify-content-between align-items-center'>
+          <span>
+            {numeral(viewCount).format('0.a')} Views . {' '}
+            {moment(publishedAt).fromNow()}
+          </span>
+
+          <div>
+            <span className='mr-3'>
+              <MdThumbUp size={26} /> {numeral(likeCount).format('0.a')}
+            </span>
+            <span className='mr-3'>
+              <MdThumbDown size={26} />{' '}
+              {numeral(dislikeCount).format('0.a')}
+            </span>
+          </div>
+        </div>
+        <div className='py-3 my-2 videoMetaData__channel d-flex justify-content-between align-items-center'>
+          <div className='d-flex'>
+            <img
+              src={channelSnippet?.thumbnails?.default?.url}
+              alt=''
+              className='mr-3 rounded-circle'
+            />
+            <div className='d-flex flex-column'>
+              <span>{channelTitle}</span>
+              <span>
+                {' '}
+                {numeral(channelStatistics?.subscriberCount).format('0.a')} {' '} subscribers
+              </span>
+            </div>
+          </div>
+
+          <button
+            className={`p-2 m-2 border-0 btn ${subscriptionStatus && 'btn-gray'}`}>
+              {subscriptionStatus ? 'Subscribed' : 'Subscribe'}
+            </button>
+        </div>
+        <div className='videoMetaData__description'>
+          <ShowMoreText
+            lines={3}
+            more='SHOW MORE'
+            less='SHOW LESS'
+            anchorClass='showMoreText'
+            expanded={false}>
+              {description}
+            </ShowMoreText>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default VideoMetaData

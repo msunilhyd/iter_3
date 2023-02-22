6775b944ca333cf55a225621c35d9d4cd40905bd
sunil <msunilhyd@gmail.com>
2023-02-21 19:13:55 -0800

    Generic Commit

--- yt-clone/src/components/categoriesBar/CategoriesBar.js
+++ yt-clone/src/components/categoriesBar/CategoriesBar.js
@@ -11,49 +11,4 @@ const keywords = [
   'React js',
   'Angular js',
   'React Native',
-  'use of API',
-  'Redux',
-  'Music',
-  'Algorith Art',
-  'Guitar',
-  'Bengali Songs',
-  'Coding',
-  'Cricket',
-  'Football',
-  'Real Madrid',
-  'Gatsby',
-  'Poor Coder',
-  'Shwetabh',
 ]
-
-const CategoriesBar = () => {
-  
-  const [activeElement, setActiveElement] = useState('All')
-
-  const dispatch = useDispatch()
-  const handleClick = value => {
-    setActiveElement(value) 
-    if (value === 'All') {
-      if (value === 'All') {
-        dispatch(getPopularVideos())
-      } else {
-        dispatch(getVideosByCategory(value))
-      }
-    }
-
-    return (
-      <div className='categoriesBar'>
-        {keywords.map((value, i) => (
-          <span 
-            onClick={() => handleClick(value)}
-            key={i}
-            className={activeElement === value ? 'active' : ''}>
-              {value}
-            </span>
-        ))}
-      </div>
-    )
-  }
-}
-
-export default CategoriesBar

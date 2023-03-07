363c8231767e13c3f566a3b3116eba923a51c01a
sunil <msunilhyd@gmail.com>
2023-03-06 22:58:51 -0800

    Generic Commit

---
+++ PracticeProject/src/main/java/PracticeClass.java
@@ -0,0 +1,21 @@
+public class PracticeClass {
+
+    public static void main(String[] args) {
+
+        printNumbers(5);
+    }
+
+    public static void printNumbers(int n) {
+
+        for (int i=0; i<=n; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static int getSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    // Write a method
+}
